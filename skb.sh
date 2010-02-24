dcf8e9a7750d347c08744047b1f0d889f2ea32ad
muflax <muflax@gmail.com>
2010-02-24 06:58:02 +0100

    skb

---
+++ skb.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# Copyright muflax <mail@muflax.com>, 2009
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+setxkbmap saneo
+
+xmodmap ~/.Xmodmap
+
+xset r rate 300 65
+xset m 3 2
