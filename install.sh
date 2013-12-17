5f9258d46eb86c8e06ca667fb4c40d6ee3f4bcaf
muflax <mail@muflax.com>
2013-12-17 17:03:30 +0000

    saneo is user-writable anyway

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 # Copyright muflax <mail@muflax.com>, 2013
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-sudo /bin/cp ./saneo /usr/share/X11/xkb/symbols/saneo
+/bin/cp ./saneo /usr/share/X11/xkb/symbols/saneo
 skb.sh
