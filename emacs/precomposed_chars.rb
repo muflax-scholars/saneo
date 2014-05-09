feac87ad21a0ef36cbfe08ef098d6845cc0bd5ce
muflax <mail@muflax.com>
2014-05-09 01:51:33 +0100

    save input as both prefix and postfix variant
    (and move emacs code into separate folder)

--- emacs/precomposed_chars.rb
+++ emacs/precomposed_chars.rb
@@ -9,14 +9,14 @@ precomposed     = Hash.new {|h, lang| h[lang] = {}}
 diacritics      = 0x0300..0x036f
 used_diacritics = Set.new
 
-File.load("saneo").each do |line|
+File.load("../saneo").each do |line|
   if line =~ /^\s*key /
     unicode_keys = line.scan(/U\d{4}/).map{|c| c[1..-1].to_i(16)}
     used_diacritics += unicode_keys.select{|c| c.in? diacritics}
   end
 end
 
-File.load("UnicodeData.txt").each do |line|
+File.load("../UnicodeData.txt").each do |line|
   code, name, type, _, _, compose, *_ = line.split(";")
 
   code = code.to_i(16)
@@ -35,15 +35,18 @@ end
 precomposed.each do |lang, precomps|
   puts "#{lang} -> #{precomps.size}"
 
-  File.save("precomposed_#{lang.downcase}.el") do |f|
-    f.puts "(quail-define-rules"
+  prefix  = File.save("precomposed_#{lang.downcase}_prefix.el")
+  postfix = File.save("precomposed_#{lang.downcase}_postfix.el")
 
-    precomps.each do |precomposed, (name, composed)|
-      precomposed = [precomposed].pack("U")
-      composed    = composed.map{|c| [c].pack("U")}.join
+  [prefix, postfix].each{|f| f.puts "(quail-define-rules"}
 
-      f.puts " (\"#{composed}\" ?#{precomposed})"
-    end
-    f.puts ")"
+  precomps.sort.each do |precomposed, (name, composed)|
+    precomposed = [precomposed].pack("U")
+    composed    = composed.map{|c| [c].pack("U")}
+
+    prefix.puts  " (\"#{composed.join}\" ?#{precomposed})"
+    postfix.puts " (\"#{composed.reverse.join}\" ?#{precomposed})"
   end
+
+  [prefix, postfix].each{|f| f.puts ")"}
 end
