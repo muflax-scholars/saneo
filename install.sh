c29e022442d2e1841c24dea312d60dd0864a8c8f
muflax <mail@muflax.com>
2014-10-06 16:35:31 +0100

    begin shifted variant

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-#!/bin/zsh
+#!/bin/zsh -l
 # Copyright muflax <mail@muflax.com>, 2013
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-/bin/cp ./saneo /usr/share/X11/xkb/symbols/saneo
-/bin/cp ./saneo_inverted /usr/share/X11/xkb/symbols/saneo_inverted
+/bin/cp ./saneo         	/usr/share/X11/xkb/symbols/saneo
+/bin/cp ./saneo_inverted	/usr/share/X11/xkb/symbols/saneo_inverted
+/bin/cp ./saneo_shifted 	/usr/share/X11/xkb/symbols/saneo_inverted
+
 skb.sh
